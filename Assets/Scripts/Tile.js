f1e2913e21d0ee05d6696e472ecef8f8dbd31dc1
Samuel Hindmarsh <Samuel.Hindmarsh@gmail.com>
2013-04-15 21:35:46 +1200

    Implemented a new tile generation system to make more cohesive worlds.
    Still not good though, so will probably redo it :(.
    
    Also, you  can't walk on water now.

--- Assets/Scripts/Tile.js
+++ Assets/Scripts/Tile.js
@@ -1,17 +1,66 @@
 #pragma strict
 
+// inspector variables
+
+var type : String;
+
 var x : int;
 var y : int;
 var isFringe : boolean;
-var type : String;
+
+// private variables
+private var probabilityOfRepetition : double;
 
 function GenerateTiles() {
-	var factory = GameObject.FindGameObjectWithTag("Global").GetComponent(TileFactory);
-
 	isFringe = false;
 	
+	var factory : TileFactory = GameObject.FindGameObjectWithTag("Global").GetComponent(TileFactory);
 	factory.GenerateTile(x,y+1); // N
 	factory.GenerateTile(x,y-1); // S
 	factory.GenerateTile(x+1,y); // E
 	factory.GenerateTile(x-1,y); // W
+}
+
+function DetermineProbabilityOfRepetition(originalProb : double) {
+	if(!(originalProb > 0.0)){
+		// we have not repeated a tile by cloning it
+		DetermineProbabilityOfRepetition();
+		return;
+	}
+	
+	switch(type){
+	case("GrassTile"):
+		probabilityOfRepetition = originalProb * 0.99;
+		break;
+	case("WaterTile"):
+		probabilityOfRepetition = originalProb * 0.95;
+		break;
+	default:
+		probabilityOfRepetition = 0.0;
+		break;
+	}
+}
+
+function DetermineProbabilityOfRepetition() {
+	switch(type){
+	case("GrassTile"):
+		probabilityOfRepetition = 0.99;
+		break;
+	case("WaterTile"):
+		probabilityOfRepetition = 0.95;
+		break;
+	default:
+		probabilityOfRepetition = 0.0;
+		break;
+	}
+	
+	print("test");
+}
+
+function GetProbabilityOfRepetition() : double{
+	return this.probabilityOfRepetition;
+}
+
+function SetProbabilityOfRepetition(prob : double){
+	probabilityOfRepetition = prob;
 }
--- Assets/Scripts/TileFactory.js
+++ Assets/Scripts/TileFactory.js
@@ -4,11 +4,13 @@ static var tileSize : int = 4;
 
 var initialDistance : int;
 var tileObjects : Tile[];
+var weights : int[];
 
 private var hashtable = new Hashtable();
 
 function Start () {
 	var start = GameObject.FindGameObjectWithTag("Respawn").GetComponent(Tile);
+	start.DetermineProbabilityOfRepetition();
 	hashtable.Add(Key(start.x, start.y), start);
 	
 	for(var x = -initialDistance; x <= initialDistance; x++){		
@@ -25,24 +27,73 @@ function Start () {
 function GenerateTile(x : int, y : int) {
 	// check if a tile already exists before creating
 	if(!hashtable[Key(x,y)]){
-		var tile : Tile = RandomTile();
+		var tile : Tile = RandomTile(x,y);
 		
-		tile.isFringe = true; // assume it's on the fringe (it probably is)
+		// assume it's on the fringe -- it probably is, and if it 
+		// isn't it'll just get marked as non-fringe later.
+		tile.isFringe = true; 
 		tile.x = x;
 		tile.y = y;
 		
 		var newTile : Tile = Instantiate(tile, new Vector3(x*tileSize, 0, y*tileSize), tile.transform.rotation);
+		newTile.DetermineProbabilityOfRepetition(tile.GetProbabilityOfRepetition());	
 		hashtable.Add(Key(x,y), newTile);
 	}
 }
 
-function RandomTile() : Tile {
-	var r : int = Random.value * tileObjects.Length;
-	var tile : Tile = tileObjects[r];
+function RandomTile(x:int, y:int) : Tile {
+	var neighbours : Tile[] = new Tile[4];
+	neighbours[0] = GetTile(x+1,y);
+	neighbours[1] = GetTile(x-1,y);
+	neighbours[2] = GetTile(x,y+1);
+	neighbours[3] = GetTile(x,y-1);
 	
+	var probCount : int = 0;
+	var probabilities : double[] = new double[4];
+	
+	for(var n : int = 0; n < neighbours.length; n++){
+		if(neighbours[n]){
+			probabilities[n] = neighbours[n].GetProbabilityOfRepetition();
+			probCount++;
+		} else {
+			// no neighbour in that direction, set probability of repetition at 0.0
+			probabilities[n] = 0.0;
+		}
+	}
+	
+	// weight the probabilities by the number of options (i.e. repeat a neighbour or create from scratch; max is 4, min is 1);
+	for(var p : int = 0; p < probabilities.length; p++){
+		if(probabilities[p] > 0.0){
+			//probabilities[p] /= probCount;
+			if(Random.value < probabilities[p]){
+				print("probabilities[p]="+probabilities[p]);
+				print("Cloning neighbour of type " + neighbours[p].gameObject.ToString());
+				return neighbours[p];
+			}
+		}
+	}
+	
+	// if we get here, we want a random type
+	var tile : Tile = RandomType(Random.value);
 	print("Using tile type " + tile.gameObject.ToString());
 	
 	return tile;
+}
+
+function RandomType(rand : double) : Tile{
+	var weightSum : int;
+	for(var w : int = 0; w < weights.length; w++){
+		weightSum += weights[w];
+	}
+	
+	var type : int = rand * weightSum;
+	for(w = 0; w < weights.length; w++){
+		type -= weights[w];
+		if(type < 0){
+			// we want this type!
+			return tileObjects[w];
+		}
+	}
 }
 
 static function Key(x : int, y : int) : String{
